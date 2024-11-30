6ab323b51024f15a1da462fdd63ae5b9dba91cb6
João Daniel <jotaf.daniel@gmail.com>
2024-11-30 09:13:06 +0100

    extract ConnectionManager

--- topic-producer/src/main/java/it/unibz/rmqshowcase/topicproducer/TopicPublisher.java
+++ topic-producer/src/main/java/it/unibz/rmqshowcase/topicproducer/TopicPublisher.java
@@ -2,8 +2,7 @@ package it.unibz.rmqshowcase.topicproducer;
 
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import it.unibz.rmqshowcase.topicproducer.rabbitmq.ConnectionManager;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -12,25 +11,19 @@ import java.util.concurrent.TimeoutException;
 public class TopicPublisher {
     private static final String EXCHANGE_NAME = "topic_exchange";
 
-    public static void main(String[] args) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("admin");
-        factory.setPassword("admin");
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionManager connectionManager = new ConnectionManager();
+        connectionManager.configure("localhost", "admin", "admin");
+        connectionManager.connect();
+        Channel channel = connectionManager.getChannel();
 
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        String routingKey = "task.urgent";
+        String message = "This is an urgent task";
 
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-            String routingKey = "task.urgent";
-            String message = "This is an urgent task";
+        channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes(StandardCharsets.UTF_8));
+        System.out.printf("Published message: '%s' with routing key '%s'%n", message, routingKey);
 
-            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.printf("Published message: '%s' with routing key '%s'%n", message, routingKey);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (TimeoutException e) {
-            throw new RuntimeException(e);
-        }
+        connectionManager.disconnect();
     }
 }
---
+++ topic-producer/src/main/java/it/unibz/rmqshowcase/topicproducer/rabbitmq/ConnectionManager.java
@@ -0,0 +1,43 @@
+package it.unibz.rmqshowcase.topicproducer.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConnectionManager {
+    private final ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
+
+    public ConnectionManager() {
+        this.factory = new ConnectionFactory();
+    }
+
+    public void configure(String url, String username, String password) {
+        this.factory.setHost(url);
+        this.factory.setUsername(username);
+        this.factory.setPassword(password);
+    }
+
+    public void connect() throws IOException, TimeoutException {
+        if (this.connection == null || this.connection.isOpen()) return;
+
+        this.connection = this.factory.newConnection();
+        this.channel = this.connection.createChannel();
+    }
+
+    public Channel getChannel() {
+        return this.channel;
+    }
+
+    public void disconnect() throws IOException, TimeoutException {
+        if (this.channel != null && this.channel.isOpen())
+            this.channel.close();
+
+        if (this.connection != null && this.connection.isOpen())
+            this.connection.close();
+    }
+}
